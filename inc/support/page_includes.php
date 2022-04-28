f355c65f00583f463d48d5304dbb43173276ebc5
Scott Bowers <scott@gomedia.co>
2022-04-28 10:48:47 +0100

    Changed favicon

--- inc/support/page_includes.php
+++ inc/support/page_includes.php
@@ -8,7 +8,8 @@
 <link rel="icon" type="image/png" href="<?= $url_website ?>img/resources/favicon/favicon-32x32.png" sizes="32x32">
 <link rel="icon" type="image/png" href="<?= $url_website ?>img/resources/favicon/favicon-16x16.png" sizes="16x16">
 <link rel="manifest" href="<?= $url_website ?>img/resources/favicon/manifest.json">
-<link rel="mask-icon" href="<?= $url_website ?>img/resources/favicon/safari-pinned-tab.svg" color="#2ecc71">
+<link rel="mask-icon" href="<?= $url_website ?>img/resources/favicon/safari-pinned-tab.svg" color="#333333">
+<meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
 <!-- Favicon -->
 <!-- User Defined Data -->
